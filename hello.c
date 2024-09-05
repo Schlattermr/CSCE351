c7153d8599d8096e21a64611b16acbcabd3db3be
schlattermr <mryansch8@gmail.com>
2024-09-05 12:21:03 -0500

    created hello world file for requirement

---
+++ hello.c
@@ -0,0 +1,10 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Hello Matt, Computer Engineering!\n");
+  printf("Matt has no partner :(\n");
+
+  return 0;
+}
