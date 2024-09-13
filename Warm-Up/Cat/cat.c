69c9c10c355dd7f23524d7858ceedc8253536735
schlattermr <mryansch8@gmail.com>
2024-09-13 11:09:40 -0500

    completed assignment

--- Warm-Up/Cat/cat.c
+++ Warm-Up/Cat/cat.c
@@ -1,19 +1,18 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
-#include <ctype.h>
 
 #define BUFFER_SIZE 100
 
 int main(int argc, char **argv) {
   char filePath[] = "data/test.txt";
-  char tempBuffer[BUFFER_SIZE];
-  char text[BUFFER_SIZE];
+  char tempBuffer[BUFFER_SIZE] = "";
+  char text[BUFFER_SIZE] = "";
 
   FILE *instream = fopen(filePath, "r");
 
   if (instream == NULL) {
-    fprintf(stderr, "Unable to open file: %s\n", "data/test.txt");
+    fprintf(stderr, "Unable to open file: %s\n", filePath);
     exit(1);
   }
 
--- Warm-Up/Echo/echo.c
+++ Warm-Up/Echo/echo.c
@@ -1,9 +1,13 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 
 int main(int argc, char **argv) {
 
-  char word[256] = argv[1];
+  char word[256];
+
+  strncpy(word, argv[1], sizeof(word) - 1);
+  word[255] = '\0';
 
   if(word == NULL) {
     printf("ERROR: No input given.\n");
