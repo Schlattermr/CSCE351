68033165f3996ad92d4daee1a90778965090a49f
Matthew Schlatter <51459912+Schlattermr@users.noreply.github.com>
2024-11-27 18:11:12 -0600

    finished FCFS reader writer problem

--- FCFS-Reader-Writer/README.md
+++ FCFS-Reader-Writer/README.md
@@ -1,2 +1,27 @@
-Team: Matthew Schlatter
+## Team: Matthew Schlatter
 
+To compile, just type "make" in the terminal. 
+
+Running with ./ReaderWriterSem_exe 0 1 0 0 1 0 0 0 0 1 returns
+the following output when tested on the Nuros server:
+
+Reader 1 starts reading
+Reader 1 ends reading
+Writer 1 starts writing
+Writer 1 ends writing
+Reader 2 starts reading
+Reader 3 starts reading
+Reader 2 ends reading
+Reader 3 ends reading
+Writer 2 starts writing
+Writer 2 ends writing
+Reader 4 starts reading
+Reader 5 starts reading
+Reader 7 starts reading
+Reader 6 starts reading
+Reader 4 ends reading
+Reader 5 ends reading
+Reader 7 ends reading
+Reader 6 ends reading
+Writer 3 starts writing
+Writer 3 ends writing
--- FCFS-Reader-Writer/ReaderWriterSem.c
+++ FCFS-Reader-Writer/ReaderWriterSem.c
@@ -2,43 +2,112 @@
 #include <stdlib.h>
 #include <pthread.h>
 #include <unistd.h>
+#include <semaphore.h>
+
+#define INPUT_SIZE 10
+
+sem_t queueSem; // Queue for FCFS Algorithm
+sem_t rwSem;
+pthread_mutex_t protectingMutex;
+int readCount = 0;
+int readerId = 1;
+int writerId = 1;
 
 void *reader() {
+    // Update id for print statement
+    pthread_mutex_lock(&protectingMutex);
+    int id = readerId++;
+    pthread_mutex_unlock(&protectingMutex);
+
+    sem_wait(&queueSem);
+
+    // Enter critial section
+    pthread_mutex_lock(&protectingMutex);
+    readCount++;
+    if (readCount == 1) {
+        sem_wait(&rwSem);
+    }
+    pthread_mutex_unlock(&protectingMutex);
+    sem_post(&queueSem);
+
+    // Simulate time elapsing
+    printf("Reader %d starts reading\n", id);
+    sleep(1);
+    printf("Reader %d ends reading\n", id);
+
+    // Exit critical section
+    pthread_mutex_lock(&protectingMutex);
+    readCount--;
+    if (readCount == 0) { // Account for last reader
+        sem_post(&rwSem);
+    }
+    pthread_mutex_unlock(&protectingMutex);
+
     return NULL;
 }
 
 void *writer() {
+    // Update id for print statement
+    pthread_mutex_lock(&protectingMutex);
+    int id = writerId++;
+    pthread_mutex_unlock(&protectingMutex);
+
+    // Enter critical section
+    sem_wait(&queueSem);
+    sem_wait(&rwSem);
+    sem_post(&queueSem);
+
+    // Simulate time elapsing
+    printf("Writer %d starts writing\n", id);
+    sleep(1); 
+    printf("Writer %d ends writing\n", id);
+
+    // Exit critical section
+    sem_post(&rwSem);
+
     return NULL;
 }
 
 int main(int argc, char *argv[]) {
-    if(argc != 11) {
-        printf("Error: Include 10 inputs (1's and 0's)");
+    if(argc != INPUT_SIZE + 1) {
+        printf("Error: Include 10 inputs (1's and 0's)\n");
+        return 0;
     }
 
     // Gather and store inputs in an array
     int b[10];
-    for(int i = 0; i < argc; i++) {
+    for(int i = 0; i < INPUT_SIZE; i++) {
         b[i] = atoi(argv[i + 1]);
     }
 
-    int arrayLength = sizeof(b) / sizeof(b[0]);
-    pthread_t threads[arrayLength];
+    // Create semaphores and mutex
+    pthread_t threads[INPUT_SIZE];
+    sem_init(&queueSem, 0, 1);
+    sem_init(&rwSem, 0, 1);
+    pthread_mutex_init(&protectingMutex, NULL);
 
     // Create the ten threads
-    for(int i = 0; i < arrayLength; i++) {
+    for(int i = 0; i < INPUT_SIZE; i++) {
+        int *id = malloc(sizeof(int));
+        *id = i + 1;
+
         if(b[i] == 0) { // reader
-            printf("Reader %d starts reading", i + 1);
-
-            pthread_create(&threads[i], NULL, &reader, NULL);
-
-            printf("Reader %d ends reading", i + 1);
+            pthread_create(&threads[i], NULL, reader, id);
         } else { // writer
-            printf("Writer %d starts writing", i + 1);
-
-            pthread_create(&threads[i], NULL, &writer, NULL);
-
-            printf("Writer %d ends writing", i + 1);
+            pthread_create(&threads[i], NULL, writer, id);
         }
+
+        free(id);
     }
+
+    for(int i = 0; i < INPUT_SIZE; i++) {
+        pthread_join(threads[i], NULL);
+    }
+
+    // Clean up
+    sem_destroy(&queueSem);
+    sem_destroy(&rwSem);
+    pthread_mutex_destroy(&protectingMutex);
+
+    return 0;
 }
